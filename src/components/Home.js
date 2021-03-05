d94aef4f6901ac8b037a5df129a2bd5902107096
MirkoGabriel <mirkogabriel67@gmail.com>
2021-03-05 20:59:12 -0300

    05/03/2021-20:59

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import logo from '../logo.png'
+import texto from '../texto.png'
 
 export default class Home extends Component {
     render() {
         return (
-            <div></div>
+            <div className="row">
+                <div className="col-md-5">
+                <img src={logo}/>
+                </div>
+                <div className="col-md-3">
+                <img src={texto}/>
+                    
+                </div>
+            </div>
         )
     }
 }
